9d3ba3e6bb0a956e789fe620537bc089fdcd6cd8
mauricioRojasSegnini <mauricio.rojassegnini@ucr.ac.cr>
2022-07-10 20:46:05 -0600

    Refactoring of the search input by key word

--- Planetarium/Scripts/filtrosNoticias.js
+++ Planetarium/Scripts/filtrosNoticias.js
@@ -9,3 +9,50 @@ function filtrarPorFuente(fuenteSeleccionada) {
         }
     });
 }
+
+
+async function main() {
+    document.getElementById("search").addEventListener("keyup", search);
+}
+
+function matchAllTitles(newsList, text) {
+    
+    var matching = new Boolean(false);
+    for (var newsTopics in newsList) {
+        for (var news in newsList[newsTopics]) {
+            matching = match(newsList[newsTopics][news].Title, text);
+            if (text == "")
+                show(newsList[newsTopics][news].Title)
+            else if (matching)
+                show(newsList[newsTopics][news].Title)
+            else
+                hide(newsList[newsTopics][news].Title)
+        }
+    }
+    return matching;
+}
+
+function matchList(list, word) {
+    var matching = new Boolean(false);
+    for (var element in list) {
+        matching = match(list[element], word);
+        if (matching)
+            return matching;
+    }
+    return matching;
+}
+
+function match(word, substring) {
+    return word.toLowerCase().includes(substring.toLowerCase());
+}
+
+function hide(elementId) {
+    //console.log("Hidding ", elementId);
+    document.getElementById(elementId).style.display = "none";
+}
+
+function show(elementId) {
+    document.getElementById(elementId).style.display = "block";
+}
+
+main();
